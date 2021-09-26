d6955416c26b9df5689fe20cafde3b2956068921
Krzysztof Ołdakowski <krz.oldakowski@gmail.com>
2021-09-26 12:07:43 +0200

    Add User model & devise endpoints

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   mount Rswag::Ui::Engine => '/api-docs'
   mount Rswag::Api::Engine => '/api-docs'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  scope :api do
+    mount_devise_token_auth_for 'User', at: 'auth'
+  end
 end
