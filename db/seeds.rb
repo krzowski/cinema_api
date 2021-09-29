e6d99902a2a28780d4b8eaf6613a9275157658a7
Krzysztof Ołdakowski <krz.oldakowski@gmail.com>
2021-09-29 07:46:47 +0200

    Update seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,3 +3,7 @@ include FactoryBot::Syntax::Methods
 user = create :user, email: 'user@example.com', password: 'password'
 admin = create :admin, email: 'admin@example.com', password: 'password'
 movie = create :fetched_movie
+pricing_category = create :movie_pricing_category
+create :movie_showing, movie: movie, movie_pricing_category: pricing_category, show_time: "10:00"
+create :movie_showing, movie: movie, movie_pricing_category: pricing_category, show_time: "12:00"
+create :movie_rating, movie: movie, user: user
